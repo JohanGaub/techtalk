dd566b282ab4b77c87cc13e25c9c487bc5da384d
JohanGaub <johangaub@gmail.com>
2023-12-21 15:58:39 +0100

    ticket-0026-sort-topic.

--- project/src/Controller/Admin/TopicCrudController.php
+++ project/src/Controller/Admin/TopicCrudController.php
@@ -12,7 +12,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -29,7 +28,7 @@ class TopicCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->hideOnForm(),
+            IdField::new('id')->hideOnForm()->setSortable(true),
             TextField::new('label'),
             TextareaField::new('description'),
 
@@ -71,6 +70,7 @@ class TopicCrudController extends AbstractCrudController
                 })->hideOnForm(),
             DateTimeField::new('inReviewAt')->hideOnForm(),
             DateTimeField::new('publishedAt')->hideOnForm(),
+            DateTimeField::new('updatedAt')->hideOnForm(),
         ];
     }
 }
