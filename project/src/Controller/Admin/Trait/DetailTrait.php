4a89bb77db9cf31d7c1696196e3bfcc0c2d66064
JohanGaub <johangaub@gmail.com>
2024-01-03 12:24:42 +0100

    ticket-0027 : fix edit duration.

--- project/src/Controller/Admin/Trait/DetailTrait.php
+++ project/src/Controller/Admin/Trait/DetailTrait.php
@@ -11,7 +11,19 @@ trait DetailTrait
     public function configureActions(Actions $actions): Actions
     {
         return $actions
-            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::INDEX)
+            ->add(Crud::PAGE_NEW, Action::INDEX)
+            ->update(Crud::PAGE_INDEX, Action::NEW, static function (Action $action) {
+                return $action->setIcon('fa-solid fa-plus')->setLabel(false);
+            })
+            ->update(Crud::PAGE_NEW, Action::INDEX, static function (Action $action) {
+                return $action->setIcon('fa-solid fa-list-ul')->setLabel(false);
+            })
+//            ->update(Crud::PAGE_NEW, Action::SHOW, function (Action $action) {
+//                return $action->setIcon('fa-solid fa-list-ul')->setLabel(false);
+//            })
+        ;
     }
 
     public function configureCrud(Crud $crud): Crud
