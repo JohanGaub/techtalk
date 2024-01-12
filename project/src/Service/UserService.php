3eaa192d219ab546409c0052c0e0747c3bae44dc
JohanGaub <johangaub@gmail.com>
2024-01-12 16:51:36 +0100

    Ticket-0046 : refactor user service.

--- project/src/Service/UserService.php
+++ project/src/Service/UserService.php
@@ -8,6 +8,8 @@ use App\Entity\Agency;
 use App\Entity\User;
 use App\Repository\AgencyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LogLevel;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 readonly class UserService
 {
@@ -15,13 +17,18 @@ readonly class UserService
         private string                 $userDirectory,
         private EntityManagerInterface $entityManager,
         private AgencyRepository       $agencyRepository,
+        private LoggerService    $loggerService
     ) {
     }
 
     public function addUsers($fileName): array
     {
+        $filePath = sprintf('%s/%s', $this->userDirectory, $fileName);
+        $openCsv = fopen($filePath, "r");
 
-        $openCsv = fopen(sprintf('%s/%s', $this->userDirectory, $fileName), "r");
+        if ($openCsv === false) {
+            throw new FileException(sprintf('Failed to open file: %s.', $filePath));
+        }
 
         // Read and discard the first row which is the header
         fgetcsv($openCsv);
@@ -31,24 +38,28 @@ readonly class UserService
         $failure = 0;
 
         while (($data = fgetcsv($openCsv)) !== false) {
+            $user = null;
 
             try {
-                $user = new User();
-                $user->setEmail(uniqid('user_') . '@gmail.com');
-                //            $user->setEmail($data[0]); // use it when we want to use every role
-                $user->setFirstName($data[1]);
-                $user->setLastName($data[2]);
-                $user->setRoles(explode(",", $data[3]));
-                $user->setAgency($this->getAgencyById($data[4]));
-                $user->setEnabled((bool) $data[5]);
-                // Set a default password because when creating user it is needed even if we don't use it.
-                // Indeed, we use the login link feature to connect to the website.
-                $user->setPassword(password_hash('default_password', PASSWORD_BCRYPT));
+                $user = $this->createUser($data);
                 $this->entityManager->persist($user);
                 ++$success;
 
-            } catch (\Exception $e) {
+                $this->loggerService->log(
+                    LogLevel::INFO,
+                    'User with email %s added successfully.',
+                    [$user->getEmail()]
+                );
+            } catch (\Exception $exception) {
                 ++$failure;
+
+                $email = $user instanceof \App\Entity\User ? $user->getEmail() : 'Unknown';
+                $this->loggerService->log(
+                    LogLevel::ERROR,
+                    'Error while adding user with email %s.',
+                    [$email],
+                    $exception
+                );
             }
 
             ++$total;
@@ -57,23 +68,40 @@ readonly class UserService
         fclose($openCsv);
         $this->entityManager->flush();
 
+        $this->loggerService->log(
+            LogLevel::INFO,
+            'Added %d users, with %d failures.',
+            [$total, $failure]
+        );
+
         return [
             'total' => $total,
             'success' => $success,
             'failure' => $failure,
         ];
+    }
 
-        //5) Renvoyer l'information de réussite ou d'échec pour chaque utilisateur
-        //6) Renvoyer le nombre d'utilisateurs ajoutés
-        //7) Renvoyer le nombre d'utilisateurs échoués
-        //8) Afficher ces informations dans une page twig
-        //        $users, $success, $failure, $total
+    private function createUser(array $data): User
+    {
+        $user = new User();
+        $user->setEmail(uniqid('user_') . '@gmail.com');
+        //            $user->setEmail($data[0]); // use it when we want to use every role
+        $user->setFirstName($data[1]);
+        $user->setLastName($data[2]);
+        $user->setRoles(explode(",", $data[3]));
+        $user->setAgency($this->getAgencyById($data[4]));
+        $user->setEnabled((bool) $data[5]);
+        /**
+         * Set a default password because when creating user it is needed even if we don't use it.
+         * Indeed, we use the login link feature to connect to the website.
+         */
+        $user->setPassword(password_hash('default_password', PASSWORD_BCRYPT));
+
+        return $user;
     }
 
     private function getAgencyById($id): ?Agency
     {
         return $this->agencyRepository->find($id);
     }
-
-
 }
