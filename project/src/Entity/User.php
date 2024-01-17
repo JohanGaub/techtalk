47d39575a589b5d44aeb513571a13c205dcc3cbd
JohanGaub <johangaub@gmail.com>
2024-01-17 11:49:51 +0100

    Ticket-0048 : update composer and user.

--- project/src/Entity/User.php
+++ project/src/Entity/User.php
@@ -13,7 +13,6 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Index(columns: ['email'], name: 'email_idx')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     use TimestampableEntity;
